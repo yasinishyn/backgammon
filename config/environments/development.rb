0328ce90fd47d29e206dc6f9e3dbfaeae40d1b39
User <user@pavliks-computer.local>
2013-03-05 01:29:44 +0200

    errorsss

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,6 +31,7 @@ AppName::Application.configure do
 
   # Do not compress assets
   config.assets.compress = false
+  config.assets.compile = true
 
   # Expands the lines which load the assets
   config.assets.debug = true
